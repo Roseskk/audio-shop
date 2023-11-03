9e745ccf6510f6fbc385925bad081a2e34155955
Roseskk <oleg419583@yandex.ru>
2023-11-04 00:45:41 +0300

    works

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 
-export default function Home() {
+export async function getData() {
+  const url = `${process.env.API}/products?&acf_format=standard&_fields=id,title,acf`;
+
+  const res = await fetch(url)
+
+  if (!res.ok) {
+    throw new Error('Failed')
+  }
+
+  return res.json()
+}
+
+
+export default async function Home() {
+  const data = await getData()
   return (
     <main>
-
+      {JSON.stringify(data[0])}
     </main>
   )
 }
+
+
+
