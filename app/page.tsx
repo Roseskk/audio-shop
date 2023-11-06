b50fe6514537dd96ebd55f3771940bc8451659db
Roseskk <oleg419583@yandex.ru>
2023-11-06 23:38:09 +0300

    audio

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Slider from "@/components/slider/slider";
 import ProductCards from "@/components/productCards/productCards";
 import Gallery from "@/components/gallery/gallery";
+import AboutAudio from "@/components/about/aboutAudio";
 
 export async function getProducts() {
     const url = `${process.env.API}/products?&acf_format=standard&_fields=id,title,acf,slug`;
@@ -25,6 +26,7 @@ export default async function Home() {
             <Slider products={products}/>
             <ProductCards productsInfo={productsCardsData}/>
             <Gallery/>
+            <AboutAudio/>
         </main>
     )
 }
---
+++ components/about/aboutAudio.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from "next/image";
+import audio from '../../assets/images/about/image-best-gear.jpg';
+
+import styles from './aboutAudio.module.scss';
+
+const AboutAudio = () => {
+    return (
+        <section className={styles.section_audio}>
+            <div className={styles.audio_content}>
+                <h2 className={styles.title}>Bringing you the <span className={styles.sub}>best</span> audio gear</h2>
+                <p className={styles.content}>
+                    Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+                    earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms
+                    available for you to browse and experience a wide range of our products. Stop by our store to meet
+                    some of the fantastic people who make Audiophile the best place to buy your portable audio
+                    equipment.
+                </p>
+            </div>
+            <Image src={audio} alt={'best_audio'}/>
+        </section>
+    )
+}
+
+export default AboutAudio;
