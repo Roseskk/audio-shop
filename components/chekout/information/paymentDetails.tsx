43c7899510feae616f4ccfc7fdd73790daaf2d73
Roseskk <oleg419583@yandex.ru>
2023-11-13 18:10:18 +0300

    payment

---
+++ components/chekout/information/paymentDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './information.module.scss';
+
+const PaymentDetails = () => {
+    return (
+        <section>
+            <h4 className={styles.checkout_sub}>PAYMENT DETAILS</h4>
+            <div>
+                <h5>Payment Method</h5>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PaymentDetails;
